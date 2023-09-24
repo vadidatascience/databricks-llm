35a4ee70cc6c4a18c55e4d97d61e8adb30c513c3
vadidatascience <62034106+vadidatascience@users.noreply.github.com>
2023-09-24 18:12:05 -0400

    Create db-llm-lab01.py

---
+++ lab01/db-llm-lab01.py
@@ -0,0 +1,9 @@
+%pip install h5py
+%pip install typing-extensions
+%pip install wheel
+
+# Spefic version will ensure when share the code to other to work without issue
+# following prompt is only need one time execution
+%pip install nlptest==1.1.0
+
+%pip install sacremoses==0.0.53
